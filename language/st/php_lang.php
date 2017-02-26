aca865311bc9870263ab557db12a373792ff39b7
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 21:51:43 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/st/php_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['php_app_description'] = 'The PHP app e fana ka lisebelisoa tsa sona taolo ya bakeng sa bothata bo itseng &#39;thekenoloji PHP ho web server ya.';
+$lang['php_app_name'] = 'PHP';
