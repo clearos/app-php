4a4a351634977c695c4e9d342515cb86385258fa
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-15 01:12:42 +0000

    Add new files and directories, commit changes, push changes

--- language/id/php_lang.php
+++ language/id/php_lang.php
@@ -1,4 +1,4 @@
 <?php
 
-$lang['php_app_description'] = 'PHP aplikasi menyediakan alat manajemen untuk teknologi PHP web server yang mendasari.';
+$lang['php_app_description'] = 'Aplikasi PHP menyediakan alat manajemen untuk teknologi server web PHP yang mendasarinya.';
 $lang['php_app_name'] = 'PHP';
