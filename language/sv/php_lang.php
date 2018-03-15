486efd62787c353a2386aaa7fed639a05e388416
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-15 01:40:22 +0000

    Add new files and directories, commit changes, push changes

--- language/sv/php_lang.php
+++ language/sv/php_lang.php
@@ -1,4 +1,4 @@
 <?php
 
-$lang['php_app_description'] = 'PHP app ger verktyg för den underliggande PHP serverteknik.';
+$lang['php_app_description'] = 'PHP-appen ger hanteringsverktyg för den underliggande PHP-webbservernekniken.';
 $lang['php_app_name'] = 'PHP';
