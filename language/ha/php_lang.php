21f7b1907083d4e2b89425c453886aa031f4a5cc
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-15 01:10:26 +0000

    Add new files and directories, commit changes, push changes

--- language/ha/php_lang.php
+++ language/ha/php_lang.php
@@ -1,4 +1,4 @@
 <?php
 
-$lang['php_app_description'] = 'The PHP app bayar management kayayyakin aiki, don tamkar PHP da sabar yanar gizo fasaha.';
+$lang['php_app_description'] = 'Kwamfuta na PHP yana samar da kayayyakin aikin gudanarwa don fasahar yanar gizon yanar gizo na PHP.';
 $lang['php_app_name'] = 'PHP';
