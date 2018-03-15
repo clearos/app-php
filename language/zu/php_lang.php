26697e47f670b5de01de000da71101b562113c28
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-15 01:50:01 +0000

    Add new files and directories, commit changes, push changes

--- language/zu/php_lang.php
+++ language/zu/php_lang.php
@@ -1,4 +1,4 @@
 <?php
 
-$lang['php_app_description'] = 'Uhlelo lokusebenza PHP inikeza amathuluzi ukuphathwa eyisisekelo PHP web server ubuchwepheshe.';
+$lang['php_app_description'] = 'Uhlelo lokusebenza lwe-PHP lunikeza amathuluzi wokuphatha we-technology yangaphakathi ye-PHP yesiphakeli sewebhu.';
 $lang['php_app_name'] = 'PHP';
