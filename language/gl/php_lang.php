38cd2536ed8b3f5e16b537d0754cbe72e49b6305
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-15 01:07:20 +0000

    Add new files and directories, commit changes, push changes

--- language/gl/php_lang.php
+++ language/gl/php_lang.php
@@ -1,4 +1,4 @@
 <?php
 
-$lang['php_app_description'] = 'A aplicación PHP ofrece ferramentas de xestión para a tecnoloxía de servidor web PHP subxacente.';
+$lang['php_app_description'] = 'A aplicación PHP fornece ferramentas de xestión para a tecnoloxía subxacente do servidor web PHP.';
 $lang['php_app_name'] = 'PHP';
