c89a492e3bee02357b0993c565b45b2202abf555
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-15 00:53:58 +0000

    Add new files and directories, commit changes, push changes

--- language/sq/php_lang.php
+++ language/sq/php_lang.php
@@ -1,4 +1,4 @@
 <?php
 
-$lang['php_app_description'] = 'App PHP siguron mjetet e menaxhimit për teknologji themelor PHP web server.';
+$lang['php_app_description'] = 'Aplikacioni PHP ofron mjete të menaxhimit për teknologjinë bazë të serverit PHP.';
 $lang['php_app_name'] = 'PHP';
