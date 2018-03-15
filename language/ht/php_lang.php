ed27c433f05fcf376bd3e6299561214924e98857
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-15 01:10:00 +0000

    Add new files and directories, commit changes, push changes

--- language/ht/php_lang.php
+++ language/ht/php_lang.php
@@ -1,4 +1,4 @@
 <?php
 
-$lang['php_app_description'] = 'App a PHP bay zouti jesyon pou teknoloji a PHP sèvè sit entènèt kache.';
+$lang['php_app_description'] = 'App a PHP bay zouti jesyon pou teknoloji a sèvè PHP kache.';
 $lang['php_app_name'] = 'PHP';
