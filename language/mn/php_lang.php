d47723f1edc00122726940333c2f0440d7f32546
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-15 01:25:14 +0000

    Add new files and directories, commit changes, push changes

--- language/mn/php_lang.php
+++ language/mn/php_lang.php
@@ -1,4 +1,4 @@
 <?php
 
-$lang['php_app_description'] = 'PHP програм суурь PHP вэб сервер технологи менежментийн хэрэгсэл болно.';
+$lang['php_app_description'] = 'PHP програм нь PHP веб серверийн технологийг удирдах хэрэгслийг хангадаг.';
 $lang['php_app_name'] = 'PHP';
