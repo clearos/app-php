e942e6687a143f863fc1ec424d571aff284f3eb0
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-15 01:48:14 +0000

    Add new files and directories, commit changes, push changes

--- language/cy/php_lang.php
+++ language/cy/php_lang.php
@@ -1,4 +1,4 @@
 <?php
 
-$lang['php_app_description'] = 'Mae&#39;r app PHP darparu offer rheoli ar gyfer y dechnoleg PHP gweinydd gwe sylfaenol.';
+$lang['php_app_description'] = 'Mae&#39;r app PHP yn darparu offer rheoli ar gyfer technoleg gweinydd gwe PHP sylfaenol.';
 $lang['php_app_name'] = 'PHP';
