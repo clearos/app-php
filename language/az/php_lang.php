852665eefd53d9acbde7efb19b11ed9fd515845f
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-15 00:55:50 +0000

    Add new files and directories, commit changes, push changes

--- language/az/php_lang.php
+++ language/az/php_lang.php
@@ -1,4 +1,4 @@
 <?php
 
-$lang['php_app_description'] = 'PHP app əsas PHP web server texnologiyası idarə tools təmin edir.';
+$lang['php_app_description'] = 'PHP proqramı əsas PHP web server texnologiyası üçün idarəetmə vasitələrini təmin edir.';
 $lang['php_app_name'] = 'PHP';
