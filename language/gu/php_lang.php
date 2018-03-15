6d3b03c22e62c5aa5d63e01323919ed62bfffe05
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-15 01:09:08 +0000

    Add new files and directories, commit changes, push changes

--- language/gu/php_lang.php
+++ language/gu/php_lang.php
@@ -1,4 +1,4 @@
 <?php
 
-$lang['php_app_description'] = 'PHP, એપ્લિકેશન અંતર્ગત PHP, વેબ સર્વર ટેકનોલોજી માટે વ્યવસ્થાપન સાધનો પૂરા પાડે છે.';
-$lang['php_app_name'] = 'PHP,';
+$lang['php_app_description'] = 'PHP એપ્લિકેશન અંતર્ગત PHP વેબ સર્વર ટેકનોલોજી માટે સંચાલન સાધનો પૂરા પાડે છે.';
+$lang['php_app_name'] = 'PHP';
