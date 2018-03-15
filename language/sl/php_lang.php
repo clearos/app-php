3529d1f217e506f92c8b10e9d4353c72603a132a
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-15 01:36:13 +0000

    Add new files and directories, commit changes, push changes

--- language/sl/php_lang.php
+++ language/sl/php_lang.php
@@ -1,4 +1,4 @@
 <?php
 
-$lang['php_app_description'] = 'Aplikacija PHP zagotavlja orodja za upravljanje s tehnologijo osnovne PHP spletni strežnik.';
+$lang['php_app_description'] = 'Aplikacija PHP nudi orodja za upravljanje za osnovno tehnologijo spletnega strežnika PHP.';
 $lang['php_app_name'] = 'PHP';
