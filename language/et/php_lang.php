dbb3b7e8d53c986187fddc9e7d50712c1dc7931a
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-15 01:05:07 +0000

    Add new files and directories, commit changes, push changes

--- language/et/php_lang.php
+++ language/et/php_lang.php
@@ -1,4 +1,4 @@
 <?php
 
-$lang['php_app_description'] = 'PHP rakendus annab juhtimisvahendeid aluseks oleva PHP web server tehnoloogiat.';
+$lang['php_app_description'] = 'PHP-rakendus pakub PHP veebiserveritehnoloogia jaoks juhtimisvahendeid.';
 $lang['php_app_name'] = 'PHP';
