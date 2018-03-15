8e48f76a5c9760cf6fc5b9d24084f9bcc61cb8ff
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-15 01:06:01 +0000

    Add new files and directories, commit changes, push changes

--- language/fi/php_lang.php
+++ language/fi/php_lang.php
@@ -1,4 +1,4 @@
 <?php
 
-$lang['php_app_description'] = 'PHP sovellus tarjoaa hallintatyökalut taustalla PHP web-palvelin teknologiaa.';
+$lang['php_app_description'] = 'PHP-sovellus tarjoaa hallintatyökaluja perustana olevalle PHP-palvelimelle.';
 $lang['php_app_name'] = 'PHP';
