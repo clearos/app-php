7be212e07cb7df7910039b483d019d125ef78327
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-15 01:07:47 +0000

    Add new files and directories, commit changes, push changes

--- language/de/php_lang.php
+++ language/de/php_lang.php
@@ -1,4 +1,4 @@
 <?php
 
-$lang['php_app_description'] = 'Die PHP-App bietet Management-Tools für die zugrunde liegende PHP-Web-Server-Technologie.';
+$lang['php_app_description'] = 'Die PHP-App bietet Management-Tools für die zugrunde liegende PHP-Webserver-Technologie.';
 $lang['php_app_name'] = 'PHP';
