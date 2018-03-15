a8ef6e26a6758159ae9b24ebc6656a4f74167a67
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-15 01:34:48 +0000

    Add new files and directories, commit changes, push changes

--- language/si/php_lang.php
+++ language/si/php_lang.php
@@ -1,4 +1,4 @@
 <?php
 
-$lang['php_app_description'] = 'එම PHP යෙදුම යටින්, PHP වෙබ් සේවාදායකය තාක්ෂණය සඳහා කළමනාකරණ මෙවලම් සපයයි.';
+$lang['php_app_description'] = 'PHP යෙදුම යටින් පවතින PHP වෙබ් සේවාදායක තාක්ෂණය සඳහා කළමනාකරණ මෙවලම් සපයයි.';
 $lang['php_app_name'] = 'PHP';
