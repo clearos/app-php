841841a4e5ac7be65acb711145929a37b527a13e
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-15 01:37:35 +0000

    Add new files and directories, commit changes, push changes

--- language/st/php_lang.php
+++ language/st/php_lang.php
@@ -1,4 +1,4 @@
 <?php
 
-$lang['php_app_description'] = 'The PHP app e fana ka lisebelisoa tsa sona taolo ya bakeng sa bothata bo itseng &#39;thekenoloji PHP ho web server ya.';
+$lang['php_app_description'] = 'Lenaneo la PHP le fana ka lisebelisoa tsa tsamaiso bakeng sa theknoloji e ka tlase ea thepa ea websaete ea PHP.';
 $lang['php_app_name'] = 'PHP';
