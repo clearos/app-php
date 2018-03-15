c5781956a560c8d43dec69379ac9a78ecf6d1789
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-15 01:37:08 +0000

    Add new files and directories, commit changes, push changes

--- language/so/php_lang.php
+++ language/so/php_lang.php
@@ -1,4 +1,4 @@
 <?php
 
-$lang['php_app_description'] = 'app PHP wuxuu bixiyaa qalab maamulka for technology PHP server web dahsoon.';
+$lang['php_app_description'] = 'Barnaamijka PHP wuxuu bixiyaa aalado maareyneed oo loogu talagalay tiknoolojiyada casriga ah ee PHP.';
 $lang['php_app_name'] = 'PHP';
