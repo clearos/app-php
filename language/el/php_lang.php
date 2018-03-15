4f632f6f1e4d347e7e9af3d7f97982644bb1d214
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-15 01:08:42 +0000

    Add new files and directories, commit changes, push changes

--- language/el/php_lang.php
+++ language/el/php_lang.php
@@ -1,4 +1,4 @@
 <?php
 
-$lang['php_app_description'] = 'Η εφαρμογή PHP παρέχει εργαλεία διαχείρισης για την υποκείμενη τεχνολογία PHP web server.';
+$lang['php_app_description'] = 'Η εφαρμογή PHP παρέχει εργαλεία διαχείρισης για την υποκείμενη τεχνολογία διακομιστή web PHP.';
 $lang['php_app_name'] = 'PHP';
