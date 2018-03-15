02636c20086aeac77cd45992c21db1f64c914896
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-15 01:27:33 +0000

    Add new files and directories, commit changes, push changes

--- language/mt/php_lang.php
+++ language/mt/php_lang.php
@@ -1,4 +1,4 @@
 <?php
 
-$lang['php_app_description'] = 'Il-app PHP tipprovdi għodod ta &#39;ġestjoni għat-teknoloġija sottostanti PHP web server.';
+$lang['php_app_description'] = 'L-applikazzjoni PHP tipprovdi għodod ta &#39;ġestjoni għat-teknoloġija tal-web server PHP sottostanti.';
 $lang['php_app_name'] = 'PHP';
