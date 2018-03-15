975e4c84aca5bab4d1e8a461dc2e38c50485abcc
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-15 01:22:30 +0000

    Add new files and directories, commit changes, push changes

--- language/mg/php_lang.php
+++ language/mg/php_lang.php
@@ -1,4 +1,4 @@
 <?php
 
-$lang['php_app_description'] = 'Ny PHP fampiharana ny fitantanana manome fitaovana ho an&#39;ny PHP fototra tranonkala mpizara teknolojia.';
+$lang['php_app_description'] = 'Ny fampiharana PHP dia manome fitaovana fitantanana ho an&#39;ny teknolojian&#39;ny tranonkala PHP.';
 $lang['php_app_name'] = 'PHP';
