70bb36c191669789ef3e5f7b5a4431cc322cf62b
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-15 01:30:18 +0000

    Add new files and directories, commit changes, push changes

--- language/pa/php_lang.php
+++ language/pa/php_lang.php
@@ -1,4 +1,4 @@
 <?php
 
-$lang['php_app_description'] = 'PHP ਐਪ ਅਧੀਨ PHP ਵੈੱਬ ਸਰਵਰ ਤਕਨਾਲੋਜੀ ਲਈ ਪਰਬੰਧ ਸੰਦ ਮੁਹੱਈਆ ਕਰਦਾ ਹੈ.';
+$lang['php_app_description'] = 'PHP ਐਪੀਐਲਪੀ ਐਪਲੀਕੇਸ਼ ਨੂੰ ਅੰਡਰਲਾਈੰਗ PHP ਵੈੱਬ ਸਰਵਰ ਤਕਨਾਲੋਜੀ ਲਈ ਪ੍ਰਬੰਧਨ ਸੰਦ ਪ੍ਰਦਾਨ ਕਰਦਾ ਹੈ.';
 $lang['php_app_name'] = 'PHP';
