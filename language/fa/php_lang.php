e1700b3cd4fb0b2bb550b6fe2179510d99583075
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-15 01:30:45 +0000

    Add new files and directories, commit changes, push changes

--- language/fa/php_lang.php
+++ language/fa/php_lang.php
@@ -1,4 +1,4 @@
 <?php
 
-$lang['php_app_description'] = 'برنامه PHP فراهم می کند ابزار مدیریت برای این فن آوری وب پی اچ پی سرور اساسی.';
-$lang['php_app_name'] = 'PHP';
+$lang['php_app_description'] = 'برنامه PHP فراهم می کند ابزار مدیریت برای فن آوری وب سایت زیر PHP است.';
+$lang['php_app_name'] = 'پی اچ پی';
