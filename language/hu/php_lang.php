5deb96e95370727f02c5ad6d7f9624df7332c493
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-15 01:11:47 +0000

    Add new files and directories, commit changes, push changes

--- language/hu/php_lang.php
+++ language/hu/php_lang.php
@@ -1,4 +1,4 @@
 <?php
 
-$lang['php_app_description'] = 'A PHP app nyújt eszközök a mögöttes PHP web szerver technológia.';
+$lang['php_app_description'] = 'A PHP alkalmazás kezelési eszközöket biztosít az alapul szolgáló PHP webszerver technológiához.';
 $lang['php_app_name'] = 'PHP';
